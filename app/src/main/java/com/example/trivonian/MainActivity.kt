8ce41572bb6cf3c22f6fefa8e44d7bc9f512d89d
Kai Adrian Danz <kai.adrian.danz@webde>
2021-01-18 13:02:00 +0100

    init

---
+++ app/src/main/java/com/example/trivonian/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.trivonian
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
