158df0c6dd2a1240d885508424df51bb761f64d7
Kai Adrian Danz <kai.adrian.danz@webde>
2021-01-18 14:41:10 +0100

    adding todo´s

--- app/src/main/java/com/example/trivonian/GameFragment.kt
+++ app/src/main/java/com/example/trivonian/GameFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.RadioButton
 import android.widget.TextView
 import org.w3c.dom.Text
 
 class GameFragment : Fragment() {
 
+
+    //TODO("Auslagern der Frage Klasse")
     data class Question(val questionText: String, val answers: List<String>, val correctAnswer: String)
 
+    //Todo("erhalt der frage mittels übergebenem objekt implementieren -> vllt komplette liste und fragenIndex?")
     val question = Question("In the Kingdom Heart series who provides the english voice for Master Eraqus?",
         listOf("Mark Hamill", "Jason Dohring", "Jesse McCartney", "Haley Joel Osment"),"Mark Hamill")
+
+    var userAnswer: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,5 +43,18 @@ class GameFragment : Fragment() {
         buttonThree.text = question.answers[2]
         val buttonFour = view.findViewById<RadioButton>(R.id.radioButton4)
         buttonFour.text = question.answers[3]
+
+        val radioButtons = listOf<RadioButton>(buttonOne, buttonTwo, buttonThree, buttonFour)
+
+
+        view.findViewById<Button>(R.id.gameFragment_nextButton).setOnClickListener {
+            for (radioButton in radioButtons) {
+                if (radioButton.isChecked) {
+                    userAnswer = radioButton.text.toString()
+                    break
+                }
+            }
+        }
+
     }
 }
