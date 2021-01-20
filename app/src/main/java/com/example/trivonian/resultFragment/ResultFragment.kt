fcda2827a593601a5a1cde7d3d7063c33674fc0f
Kai Adrian Danz <kai.adrian.danz@webde>
2021-01-20 12:25:53 +0100

    start of implementing data-binding in ResultFragment

--- app/src/main/java/com/example/trivonian/resultFragment/ResultFragment.kt
+++ app/src/main/java/com/example/trivonian/resultFragment/ResultFragment.kt
@@ -5,20 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trivonian.R
+import com.example.trivonian.databinding.FragmentResultBinding
 
 
 class ResultFragment : Fragment() {
 
     lateinit var recyclerView: RecyclerView
+    lateinit var binding: FragmentResultBinding
+    lateinit var viewModel: ResultFragmentViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_result, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_result, container, false)
+        viewModel = ViewModelProvider(this).get(ResultFragmentViewModel::class.java)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/trivonian/resultFragment/ResultFragmentViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.trivonian.resultFragment
+
+import androidx.lifecycle.ViewModel
+
+class ResultFragmentViewModel : ViewModel() {
+
+
+}
