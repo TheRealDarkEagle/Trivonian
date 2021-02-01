acd5460f80a042206c8e60978ae48e77b66a2935
Kai Adrian Danz <kai.adrian.danz@webde>
2021-02-01 13:04:13 +0100

    removing unused imports

--- app/src/main/java/com/example/trivonian/resultFragment/ResultAdapter.kt
+++ app/src/main/java/com/example/trivonian/resultFragment/ResultAdapter.kt
@@ -1,23 +1,18 @@
 package com.example.trivonian.resultFragment
 
-import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trivonian.R
 import com.example.trivonian.databinding.TextItemViewBinding
 import com.example.trivonian.dataclasses.Question
-import org.w3c.dom.Text
 
 class ResultAdapter(
     private val questions: List<Question>,
     private val userAnswers: List<String>
 ) : RecyclerView.Adapter<ResultAdapter.ViewHolder>() {
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
