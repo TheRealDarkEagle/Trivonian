005308f2451f403afaab0e7d96ca0d21435ab316
Kai Adrian Danz <kai.adrian.danz@webde>
2021-02-02 10:24:19 +0100

    adding navigation back to startFragment

--- app/src/main/java/com/example/trivonian/resultFragment/ResultFragment.kt
+++ app/src/main/java/com/example/trivonian/resultFragment/ResultFragment.kt
@@ -9,6 +9,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.RecyclerView
 import com.example.trivonian.R
@@ -37,11 +38,20 @@ class ResultFragment : Fragment(), Logable {
         val userAnswers = viewModel.answerList.value
         val questions = viewModel.questionList.value
 
+        binding.playAgainButton.setOnClickListener {
+            navigateToStartFragment()
+        }
+
         if (questions != null && userAnswers != null) {
             logInformation("starting resultadapter with -> ${userAnswers.size} & ${questions.size}")
             val adapter = ResultAdapter(questions, userAnswers)
             recyclerView.adapter = adapter
         }
+    }
+
+    private fun navigateToStartFragment() {
+        val action = ResultFragmentDirections.actionResultFragmentToStartFragment()
+        findNavController().navigate(action)
     }
 
 }
